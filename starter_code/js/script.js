d21784c91037fa6dbcf2257a516b2a2190e722e8
tgeorge27 <tgeorge@cristoreyny.org>
2016-02-04 13:49:05 +0000

    flickr

--- starter_code/js/script.js
+++ starter_code/js/script.js
@@ -1,6 +1,18 @@
 // YOUR CODE GOES HERE
-var clientID = "REPLACE ME WITH CLIENT ID";
+var clientID = "36aa5c6b3029e11d9b4daeea8da6276e";
 
 $(document).ready(function() {
-    // your code here
+
+    $("#search-btn").click(function() {
+        var userInput = $("#search-query").val();
+        searchFlickr(clientID, userInput, 10, function(photos) {
+                 $('#search-results').empty();
+                  for (var i = 0; i < photos.photo.length; i++) {
+                $('#search-results').append('<img src=' + photos.photo[i].url_m + ' >');
+            };
+        });
+    });
+    $("#reset-btn").click(function() {
+        $('#search-results').empty();
+    });
 });
